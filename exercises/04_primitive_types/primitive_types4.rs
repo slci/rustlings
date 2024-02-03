c51c3fa8bb1770e4f97695e44ec220fe57bd8b01
Sławomir Cielepak <slawomir.cielepak@gmail.com>
2024-02-03 10:09:27 +0100

    progress 01-04

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,15 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    for idx in 0..a.len()+2 {
+        println!("{:?}", a.get(idx))
+    }
+
+    let nice_slice = &a[1..4];
 
     assert_eq!([2, 3, 4], nice_slice)
 }
